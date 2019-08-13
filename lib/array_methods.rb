a4778df13228c6a850568b488b204a1debed12bc
Andrew Thiessen <squaids449@gmail.com>
2019-08-13 22:50:40 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,16 +9,13 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  num = 0
-  while num < array.count
-    num += 1
-    if array[num] > array[num + 1]
-      nil
-    end
-    if array [num] < array[num + 1]
-      
-    end
-  end
+  # num = 0
+  array.sort!
+  print array.last
+  # while num < array.count
+  #   num += 1
+    
+  # end
 end
 
 def find_min_value(array)
