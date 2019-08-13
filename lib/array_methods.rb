bcf5fd027c4ba1815e590bf78f8121694ed46290
Andrew Thiessen <squaids449@gmail.com>
2019-08-13 22:40:39 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -13,6 +13,9 @@ def find_max_value(array)
   while num < array.count
     num += 1
     if array[num] > array[num + 1]
+      nil
+    end
+    if array [num] < array[num + 1]
       
     end
   end
